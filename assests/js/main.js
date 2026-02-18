640a7ee660a8ab5f84d2e612c6cc7c5b633c7c04
l24200292 píxeles <l24200292@pachuca.tecnm.mx>
2026-02-18 16:38:13 -0600

    Se agrega tringulo y carita 2d

--- assests/js/main.js
+++ assests/js/main.js
@@ -23,29 +23,46 @@ function draw() {
   ctx.fillStyle = "#2dd4bf";
   ctx.fillRect(25, 25, 100, 100);
 
-  // =============================
-  // LIMPIEZA INTERNA
-  // =============================
   ctx.clearRect(45, 45, 60, 60);
 
-  // =============================
-  // CONTORNO
-  // =============================
   ctx.strokeStyle = "#111827";
   ctx.lineWidth = 3;
   ctx.strokeRect(50, 50, 50, 50);
 
   // =============================
-  // TRIÁNGULO (Path API)
+  // TRIÁNGULO
   // =============================
   ctx.beginPath();
   ctx.moveTo(200, 100);
   ctx.lineTo(250, 150);
   ctx.lineTo(250, 50);
   ctx.closePath();
-
   ctx.fillStyle = "#f97316";
   ctx.fill();
+
+  // =============================
+  // CARITA CON arc()
+  // =============================
+  ctx.beginPath();
+
+  // Círculo externo
+  ctx.arc(400, 100, 50, 0, Math.PI * 2, true);
+
+  // Boca
+  ctx.moveTo(435, 100);
+  ctx.arc(400, 100, 35, 0, Math.PI, false);
+
+  // Ojo izquierdo
+  ctx.moveTo(390, 90);
+  ctx.arc(385, 90, 5, 0, Math.PI * 2, true);
+
+  // Ojo derecho
+  ctx.moveTo(420, 90);
+  ctx.arc(415, 90, 5, 0, Math.PI * 2, true);
+
+  ctx.strokeStyle = "#1e293b";
+  ctx.lineWidth = 2;
+  ctx.stroke();
 }
 
 function initUI() {
